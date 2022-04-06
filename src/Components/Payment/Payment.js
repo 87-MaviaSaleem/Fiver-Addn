baa2bf857022a5806651dc65642574396ea973b0
SheikhUmar2476 <80356967+SheikhUmar2476@users.noreply.github.com>
2022-04-06 21:29:23 +0500

    updated

--- src/Components/Payment/Payment.js
+++ src/Components/Payment/Payment.js
@@ -1,8 +1,65 @@
 import { React, useEffect, useState } from "react";
 import "./Payment.css";
 import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 
 function PaymentScreen() {
+  const [buyer, setBuyer] = useState({
+    key: uuidv4(),
+    balance: 0,
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    console.log(event.target.value);
+
+    setBuyer((prevValue) => {
+      return { ...prevValue, [name]: value };
+    });
+    console.log("sheikh change");
+  }
+
+  function handleSubmit1(e) {
+    const user = {
+      buyer: buyer,
+      value: 1,
+    };
+    console.log(user.buyer);
+    axios.post("http://localhost:8000", user).then((result) => {
+      console.log("successfully posted");
+      setBuyer({
+        key: "",
+        balance: "",
+      });
+    });
+  }
+
+  function handleSubmit2(e) {
+    if (buyer.key != "") {
+      const user = {
+        buyer: buyer,
+        value: 2,
+      };
+      axios.post("http://localhost:8000", user).then((result) => {
+        if (result) {
+          console.log("successfully login");
+          setBuyer({
+            key: buyer.key,
+            balance: buyer.balance,
+          });
+        }
+      });
+    }
+  }
+
+  function copy() {
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copyText.value);
+    alert("Copied the text: " + copyText.value);
+  }
+
   function handleRefill() {
     var amount = document.getElementById("amount").value;
     var method = document.getElementById("method").value;
@@ -54,8 +111,33 @@ function PaymentScreen() {
   }
   return (
     <div className="payment-container" id="payment">
-      <div className="payment-bar1">
-        <p>Enter amount to add in your account</p>
+      <div>
+        <form>
+          <h1>Get/Put Your Key</h1>
+          <div>
+            <input
+              value={buyer.key}
+              name="key"
+              id="myInput"
+              onChange={handleChange}
+              type="text"
+              placeholder="key"
+            />
+
+            <button className="ml-4 refill-btn1" onClick={copy}>
+              C
+            </button>
+          </div>
+
+          <button className="mr-4 mt-4 refill-btn" onClick={handleSubmit1}>
+            Generate
+          </button>
+          <button className=" mt-4 refill-btn" onClick={handleSubmit2}>
+            Connect
+          </button>
+
+          <p className="mt-4"> balance = {buyer.balance}</p>
+        </form>
       </div>
       <div className="payment-box">
         <div className="payment-bar">
